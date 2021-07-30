3a6310ce4064d1194fb3ce0fedb9332484e5ed8e
530844945@qq.com <530844945@qq.com>
2021-07-30 22:18:30 +0800

    add css,scss,hotloading

--- webpack实践练习/webpack.config.js
+++ webpack实践练习/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test:/\.s?css$/i,
+        test:/\.(s[ac]|c)ss$/i,
         use:[MiniCssExtractPlugin.loader,"css-loader","postcss-loader","sass-loader"]
       },
       {
